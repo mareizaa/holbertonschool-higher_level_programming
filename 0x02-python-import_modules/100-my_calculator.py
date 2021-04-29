b1e9fc713dccfb93ea2043925b20536df592c05c
mareizaa <marce.3210@gmail.com>
2021-04-28 20:36:46 -0500

    calculadora

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 from calculator_1 import add, sub, mul, div
 import sys
 if __name__ == "__main__":
-    if len(sys.argv) is not 4:
+    if (len(sys.argv) - 1) is not 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(sys.argv[1])
