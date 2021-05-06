0bc8a7f8b2d507392efd8e3dc85ca8b494d4b260
mareizaa <marce.3210@gmail.com>
2021-05-05 22:09:30 -0500

    print

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for width in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][width]), end="")
-                if width != len(matrix) - 1:
-                    print(" ", end="")
-        print()
+    for i in matrix:
+        print(" ".join("{:d}".format(width) for width in i))
