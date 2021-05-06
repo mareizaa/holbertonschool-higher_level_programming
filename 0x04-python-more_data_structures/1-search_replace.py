a352be447471c7814ca33fc61a2a335b29b93ed3
mareizaa <marce.3210@gmail.com>
2021-05-06 18:32:22 -0500

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     copy = my_list.copy()
     for i in range(len(my_list)):
         if copy[i] == search:
-            copy[i]= replace
+            copy[i] = replace
     return copy
