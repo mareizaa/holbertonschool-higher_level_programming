403adf0ab7c41354e000876169a60b1067ecab35
mareizaa <marce.3210@gmail.com>
2021-05-06 17:33:43 -0500

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    copy = []
+    for i in matrix:
+        row = []
+        for j in i:
+            row.append(j ** 2)
+        copy.append(row)
+    return new_mt
