0bd5621d24ddff131c5c16b5f2eda84ca0c11814
mareizaa <marce.3210@gmail.com>
2021-05-18 18:45:03 -0500

    square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Este es el docstring del módulo"""
+
+
+class Square:
+    """Este es el docstring de la clase"""
+    def __init__(self, size=0):
+        """Este es el docstring de la instancia"""
+        if isinstance(size, int):
+            self.__size = size
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """returns the current square area"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """returns the current square area"""
+        if isinstance(value, int):
+            self.__size = value
+            if value < 0:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
