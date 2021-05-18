1dd45ce93c3f77367fc9b93f3ead2fecf074a4bc
mareizaa <marce.3210@gmail.com>
2021-05-18 16:11:20 -0500

    up

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Este es el docstring del módulo"""
+
+
+class Square:
+    """Este es el docstring de la clase"""
+    def __init__(self, size):
+        """Este es el docstring de la instancia"""
+        self.__size = size
