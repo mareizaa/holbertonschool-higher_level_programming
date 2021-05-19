38d9af6eb309c54dcefb117f54ec6884689dcb7e
mareizaa <marce.3210@gmail.com>
2021-05-19 17:12:22 -0500

    up

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as e:
-        print("Exeption: {}".format(e), file=sys.stderr)
+        print("Exeption: {:}".format(e), file=sys.stderr)
         return False
