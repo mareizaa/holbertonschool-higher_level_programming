8a8aa4409b19043dc5159fe33b966625f073c5a2
mareizaa <marce.3210@gmail.com>
2021-05-26 07:34:48 -0500

    div

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Divide each element of the matrix 
+Divide each element of the matrix
 by a given number
 returns each position / div
 """
@@ -10,6 +10,7 @@ def matrix_divided(matrix, div):
     """Divide each element of the matrix"""
     new = []
     length = len(matrix[0])
+    Error = "matrix must be a matrix (list of lists) of integers/floats"
 
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
@@ -18,7 +19,8 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     if type(matrix) is not list:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     for i in matrix:
         raw = []
@@ -28,7 +30,7 @@ def matrix_divided(matrix, div):
                     result = value / div
                     raw.append(round(result, 2))
                 else:
-                    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    raise TypeError(Error)
             new.append(raw)
         else:
             raise TypeError("Each row of the matrix must have the same size")
