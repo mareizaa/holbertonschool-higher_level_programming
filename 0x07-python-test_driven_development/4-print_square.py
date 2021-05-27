4e98282ad2ef9895642bcce4d3bbae42baf1ce7c
mareizaa <marce.3210@gmail.com>
2021-05-26 19:44:35 -0500

    square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+print square
+using the # character
+
+"""
+
+
+def print_square(size):
+    """
+    print square
+    using the # character
+
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size <= 0 and type(size) is float:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
+    
