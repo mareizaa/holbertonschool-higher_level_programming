00a62ef5d60c2a668733bcbb5dba8afeea987205
mareizaa <marce.3210@gmail.com>
2021-06-01 18:32:28 -0500

    mylist

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """
+    class MyList that inherits from list
+    """
+    def print_sorted(self):
+        """
+        prints the list
+        """
+        print(sorted(self))
