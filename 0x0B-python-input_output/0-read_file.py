8f75b11e10f502fc385823c9c2ee9cda9b1eb282
mareizaa <marce.3210@gmail.com>
2021-06-02 16:54:05 -0500

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename) as f:
+    """ reads a text file (UTF8) and prints it"""
+    with open(filename, encoding = 'utf-8') as f:
         print(f.read(), end="")
