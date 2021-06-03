b33b7f579b38683319aac364d05f3cc26e567944
mareizaa <marce.3210@gmail.com>
2021-06-02 21:00:26 -0500

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,6 +3,11 @@
 import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+arg = sys.argv[1:]
 filename = "add_item.json"
-load_from_json_file(filename)
-save_to_json_file(sys.argv[1:], filename)
+obj = load_from_json_file(filename)
+for i in arg:
+    obj.append(i)
+save_to_json_file(obj, filename)
