cce585c95b4f6ee0d37bd08cf029928fb6dd1f11
mareizaa <marce.3210@gmail.com>
2021-06-03 21:12:04 -0500

    compare

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,12 +11,13 @@ class Student():
         self.age = age
 
     def to_json(self, attrs):
-        if type(attrs) is not list:
+        if type(attrs) is not list or attrs is None:
             return self.__dict__
         new = {}
         for i in attrs:
             if type(i) is not str:
-                return (self.__dict__)
-            if i in self.__dict__:
-                new[i] = self.__dict__[i]
+                return self.__dict__
+            else:
+                if i in self.__dict__:
+                    new[i] = self.__dict__[i]
         return new
