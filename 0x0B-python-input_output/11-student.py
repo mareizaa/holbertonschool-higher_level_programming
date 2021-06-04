ecb7ce2c5930618574a775fb7d8c0e9a2cad7fca
mareizaa <marce.3210@gmail.com>
2021-06-03 21:37:22 -0500

    student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Create a Class"""
+
+
+class Student():
+    """Class student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is not list or attrs is None:
+            return self.__dict__
+        new = {}
+        for i in attrs:
+            if type(i) is not str:
+                return self.__dict__
+            if i in self.__dict__:
+                new[i] = self.__dict__[i]
+        return new
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
