0188797a0eed34402f8e21b9915572129832f764
mareizaa <marce.3210@gmail.com>
2021-06-08 16:12:11 -0500

    test2

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Unittest for Rectangle
+"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ Test class Rectangle """
+    def test_rectangle(self):
+        """ Test attributes """
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        r2 = Rectangle(8, 15)
+        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.width, 8)
+        self.assertEqual(r2.height, 15)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+        r3 = Rectangle(8, 15, 5, 4, 6)
+        self.assertEqual(r3.id, 6)
+        self.assertEqual(r3.width, 8)
+        self.assertEqual(r3.height, 15)
+        self.assertEqual(r3.x, 5)
+        self.assertEqual(r3.y, 4)
+
+    def ErrorType(self):
+        r = Rectangle("x", 2)
+        self.assertRaises(TypeError, r.width)
+
+    def ErrorValue(self):
+        r1 = Rectangle(-0, 2)
+        self.assertRaises(ValueError, r1.width)
+        r2 = Rectangle(8, 15, -5, 4, 6)
+        self.assertRaises(ValueError, r2.x)
+
+    def area(self):
+        r = Rectangle(3, 2)
+        self.assertEqual(r.area(), 6)
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.area(), 20)
