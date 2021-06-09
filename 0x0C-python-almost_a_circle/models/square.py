0b372d57702452e6c9963e469a13a4f2f1a30f14
mareizaa <marce.3210@gmail.com>
2021-06-08 21:30:22 -0500

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,12 +7,10 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """
-    class square that inherits from Rectangle
+    class square from Rectangle
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        class constructor
-        """
+        """ class constructor Square """
         super().__init__(size, size, x, y, id)
         self.size = size
 
@@ -24,20 +22,20 @@ class Square(Rectangle):
     def size(self, size):
         self.width = size
         self.height = size
-    
+
     def __str__(self):
         return str("[Square] ({}) {}/{} - {}".format(
                 self.id, self.x, self.y, self.width))
 
     def update(self, *args, **kwargs):
-        if args and args != None:
+        if args:
             if len(args) == 1:
                 self.id = args[0]
-            elif len(args) == 2:
+            if len(args) == 2:
                 self.size = args[1]
-            elif len(args) == 3:
+            if len(args) == 3:
                 self.x = args[2]
-            elif len(args) == 4:
+            if len(args) == 4:
                 self.y = args[3]
 
         for key, value in kwargs.items():
