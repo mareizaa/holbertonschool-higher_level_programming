f78f7f3859007c36cd5a4f680c1a38cfe214818f
mareizaa <marce.3210@gmail.com>
2021-06-09 11:41:19 -0500

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """
-Unittest for Rectangle
+Unittest for Rectangle inherits from Base
 """
 import unittest
 from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    """ Test class Rectangle """
+    """ Test class Rectangle inherits from Base"""
     def test_rectangle(self):
         """ Test attributes """
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.id, 7)
         self.assertEqual(r1.width, 10)
         self.assertEqual(r1.height, 2)
         self.assertEqual(r1.x, 0)
         self.assertEqual(r1.y, 0)
         r2 = Rectangle(8, 15)
-        self.assertEqual(r2.id, 2)
+        self.assertEqual(r2.id, 8)
         self.assertEqual(r2.width, 8)
         self.assertEqual(r2.height, 15)
         self.assertEqual(r2.x, 0)
@@ -31,15 +31,17 @@ class TestRectangle(unittest.TestCase):
 
     def test_ErrorType(self):
         """ Test attributes """
-        r = Rectangle("x", 2)
-        self.assertRaises(TypeError, r.width)
+        with self.assertRaises(TypeError) as error:
+            r = Rectangle("x", 2)
+        self.assertEqual("width must be an integer", str(error.exception))
 
     def test_ErrorValue(self):
         """ Test attributes """
-        r1 = Rectangle(-0, 2)
-        self.assertRaises(ValueError, r1.width)
-        r2 = Rectangle(8, 15, -5, 4, 6)
-        self.assertRaises(ValueError, r2.x)
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(-0, 2)
+        with self.assertRaises(ValueError):
+            r2 = Rectangle(8, 15, -5, 4, 6)
+        
 
     def test_area(self):
         """ Test attributes """
