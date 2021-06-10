2a656971fdab2035a6e9336787d4aa4b3ff1effe
mareizaa <marce.3210@gmail.com>
2021-06-10 16:38:13 -0500

    staticmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,9 @@ class Base:
                 new_list.append(i.to_dictionary())
         with open("{}.json".format(cls.__name__), 'w') as f:
             f.write(cls.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
