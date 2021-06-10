fc3efa82f1ba21a8d67b150385f9f08d7b58d563
mareizaa <marce.3210@gmail.com>
2021-06-10 16:24:35 -0500

    classmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,3 +24,14 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        if list_objs is None:
+            empty = []
+        new_list = []
+        for i in list_objs:
+            new_list.append(i.to_dictionary())
+        with open("{}.json".format(cls.__name__), 'w', encoding='utf-8') as f:
+            f.write(cls.to_json_string(new_list))
