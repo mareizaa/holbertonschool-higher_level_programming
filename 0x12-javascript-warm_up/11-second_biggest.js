13f6c0f9881a2cd0f1a4036a5cebd21029603b8a
mareizaa <marce.3210@gmail.com>
2021-08-02 22:16:43 -0500

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ if (process.argv.length === 2) {
   console.log(0);
 } else {
   const aSet = process.argv.slice(2);
-  const aSet1 = aSet.sort(function(a, b){return a - b});
+  const aSet1 = aSet.sort(function (a, b) { return a - b; });
   const len = aSet1.length - 1;
   const second = len - 1;
   console.log(aSet1[second]);
