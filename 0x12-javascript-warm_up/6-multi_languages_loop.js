b0f3c2d89267df074ff4017b6ea0324b0b3b0705
mareizaa <marce.3210@gmail.com>
2021-08-02 19:24:38 -0500

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const myvar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < myvar.length; i++) {
+let i;
+for (i = 0; i < myvar.length; i++) {
   console.log(myvar[i]);
 }
