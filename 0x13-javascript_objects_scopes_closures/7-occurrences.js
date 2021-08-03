685d863d6d664f3917b6e39ca75770ab92f5205d
mareizaa <marce.3210@gmail.com>
2021-08-03 15:43:23 -0500

    task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let i; let count = 0;
+  for (i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      count++;
+    }
+  }
+  return (count);
+};
