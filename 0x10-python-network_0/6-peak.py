46ca6f2223bdfa78de2dbe2cbbf535c0897123b2
mareizaa <marce.3210@gmail.com>
2021-08-19 21:50:29 -0500

    task 6 correction

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,8 +7,6 @@ def find_peak(list_of_integers):
 
     if length <= 1:
         return None
-    if list_of_integers[0] > list_of_integers[1]:
-        return list_of_integers[0]
 
     for i in range(1, length - 1):
         if (list_of_integers[i] >= list_of_integers[i - 1] and
