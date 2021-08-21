952cc6f7a584b43bd2a1edb33c9218a92b292249
mareizaa <marce.3210@gmail.com>
2021-08-20 20:57:13 -0500

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,9 @@
 import urllib.request
 
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print("Body response:\n\t- type:", type(html))
-    print("\t- content:", html)
-    print("\t- utf8 content:", html.decode('UTF-8'))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:\n\t- type:", type(html))
+        print("\t- content:", html)
+        print("\t- utf8 content:", html.decode('UTF-8'))
