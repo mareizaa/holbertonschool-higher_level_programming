9e004377bec6ea05768a3f966227c9386112ddc8
mareizaa <marce.3210@gmail.com>
2021-08-20 20:40:35 -0500

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-# Fetches https://intranet.hbtn.io/status
+"""Fetches https://intranet.hbtn.io/status"""
 import urllib.request
 
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode('UTF-8'))
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print("Body response:\n\t- type:", type(html))
+    print("\t- content:", html)
+    print("\t- utf8 content:", html.decode('UTF-8'))
