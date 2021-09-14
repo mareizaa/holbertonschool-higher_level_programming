8fc7c17701857c96f793c57e2c376d8a4db96098
marareizaa <marce.3210@gmail.com>
2021-09-14 22:19:46 +0000

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,8 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], (err) => {
-  if (err) throw err;
-
-  console.log('Success!');
+  if (err) {
+    console.error(err)
+    return
+  }
 });
