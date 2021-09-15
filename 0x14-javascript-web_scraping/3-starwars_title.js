2f8a19481b7ded4cebf978961fdf2ce42f869c91
mareizaa <marce.3210@gmail.com>
2021-09-14 19:44:02 -0500

    task3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+
+request(url, (error, data, body) => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log(JSON.parse(body).title);
+});
