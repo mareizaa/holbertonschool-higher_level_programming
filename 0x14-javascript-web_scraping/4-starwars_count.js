11ddf5d4dfd4bce19c90cde4a0d96cb637b8477d
mareizaa <marce.3210@gmail.com>
2021-09-14 22:01:01 -0500

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,8 @@
 
 const request = require('request');
 const url = process.argv[2];
-const id = 'https://swapi-api.hbtn.io/api/people/' + '18/';
+const id = '18';
+const char = 'https://swapi-api.hbtn.io/api/people/'.concat(id, '/');
 
 request(url, (error, data, body) => {
   if (error) {
@@ -12,7 +13,7 @@ request(url, (error, data, body) => {
   const films = obj.results;
   let num = 0;
   for (let i = 0; i < films.length; i++) {
-    if (films[i].characters.includes(id)) {
+    if (films[i].characters.includes(char)) {
       num += 1;
     }
   }
